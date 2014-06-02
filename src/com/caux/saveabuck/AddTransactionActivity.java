aee74ace99628a4a479ab5615eaec88e3016b60d
Tiago de Caux <tiago.caux@gmail.com>
2014-06-02 20:02:24 -0300

    - Add Transaction to DB

--- src/com/caux/saveabuck/AddTransactionActivity.java
+++ src/com/caux/saveabuck/AddTransactionActivity.java
@@ -1,5 +1,6 @@
 package com.caux.saveabuck;
 
+import com.caux.saveabuck.db.SaveABuckData;
 import com.caux.saveabuck.model.Transaction;
 import com.example.saveabuck.R;
 
@@ -10,15 +11,20 @@ import android.view.WindowManager.LayoutParams;
 import android.widget.EditText;
 
 public class AddTransactionActivity extends Activity {
+	
+	protected SaveABuckData DB;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_transaction);
 		
-		EditText editText = (EditText) findViewById(R.id.editTextValue);
+		// Initialize the DB
+		DB = new SaveABuckData(AddTransactionActivity.this);
+		
         // Request focus and show soft keyboard automatically
-        editText.requestFocus();
+		EditText editText = (EditText) findViewById(R.id.editTextValue);
+		editText.requestFocus();
         this.getWindow().setSoftInputMode(LayoutParams.SOFT_INPUT_STATE_VISIBLE);		
 		
 	}
@@ -27,11 +33,13 @@ public class AddTransactionActivity extends Activity {
 	public void buttonAddTransaction(View view) {
 		EditText editText = (EditText) findViewById(R.id.editTextValue);
 		
+		
 		Double value = Double.valueOf(editText.getText().toString());
 		
 		Transaction aTransaction = new Transaction(0, 0, value);
 		
-		
+		DB.storeTransaction(aTransaction);
+	    this.finish();			
 	}	
 	
 	/** Called when the user clicks the Cancel button */
